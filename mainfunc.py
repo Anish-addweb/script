e5c4098aa7968471c9ad9874aa02b3791b58360c
anish <anish.addweb@gmail.com>
2018-06-20 15:20:55 +0530

    Update

---
+++ mainfunc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+def main():
+  print("Hello World!")
+
+
+main()
+
+if __name__== "__main__":
+  main()
+
+print("Darklord")
+
+
+var1 = "Guru99!"
+var2 = "Software Testing"
+print("var1[0]:",var1[0])
+print("var2[1:5]:",var2[1:5])
+
+word="darklord career guru99"		
+print(word.split(' '))
+
+word="darklord career guru99"		
+print(word.split('r'))
+
+tup1 = ('Robert', 'Carlos','1965','Terminator 1995', 'Actor','Florida');
+tup2 = (1,2,3,4,5,6,7);
+print(tup1[0])
+print(tup2[1:3])
+
+
+x = ("darklord", 20, "Education")    # tuple packing
+(company, emp, profile) = x    # tuple unpacking
+print(company)
+print(emp)
+print(profile)
+
+
+a=(5,6)
+b=(1,4)
+if (a>b):print("a is bigger")
+else: print("b is bigger")
